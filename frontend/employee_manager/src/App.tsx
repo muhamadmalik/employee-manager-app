2ede3ca5c67e8614e0c87e0c7e084a3ca46a08f8
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-09-26 13:33:56 +0500

    add access simulation feature with file upload and axios integration

--- frontend/employee_manager/src/App.tsx
+++ frontend/employee_manager/src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Employee {
+  id: string;
+  access_level: number;
+  request_time: string;
+  room: string;
 }
 
+interface Result {
+  id: string;
+  status: "Granted" | "Denied";
+  reason: string;
+}
+
+
+
+function App() {
+  const [file, setFile] = useState<File | null>(null);
+  const [results, setResults] = useState<Result[]>([]);
+
+
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleSimulate = async () => {
+    if (!file) return;
+
+    const text = await file.text();
+    const employees: Employee[] = JSON.parse(text);
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/access/simulate", {
+        employees,
+      });
+      setResults(res.data);
+    } catch (err) {
+      console.error("Error sending file:", err);
+    }
+  };
+
+
+  return (
+    <div >
+      <h1>Access Simulator</h1>
+      <input type="file" accept=".json" onChange={handleFileChange} />
+      <button onClick={handleSimulate} disabled={!file}>
+        Simulate Access
+      </button>
+
+      {results.length > 0 && (
+        <div style={{ marginTop: "20px" }}>
+          <h2>Results</h2>
+          <ul>
+            {results.map((r, i) => (
+              <li key={i}>
+                <b>{r.id}</b> → {r.status} ({r.reason})
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
 export default App
