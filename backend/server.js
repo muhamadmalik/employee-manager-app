b25c4a85c86282381a49c66a242c2398f107c622
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-09-26 11:26:03 +0500

    enabled cors config with wild

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json());
 
 
-const PORT = 3000;
+app.get('/test', (req, res) => {
+    res.json("this is the live server.")
+})
+
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
